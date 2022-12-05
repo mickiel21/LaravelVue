9b8d7b7ac5795c580b492a787ca81e09cdea78df
Mickiel F. Oraa <mforaa@smg.local>
2022-12-06 00:38:05 +0800

    models and migration

--- database/migrations/2022_12_05_163429_create_clients_table.php
+++ database/migrations/2022_12_05_163429_create_clients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateClientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->string('password');
             $table->string('first_name');
             $table->string('last_name');
-            $table->integer('contact_no');
-            $table->date('birth_date');
-            $table->integer('role_id')->unsigned()->nullable()->default(0);
-            $table->rememberToken();
+            $table->string('contact_no');
+            $table->date('birthday');
+            $table->integer('interest_id');
             $table->timestamps();
         });
     }
@@ -34,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('clients');
     }
 }
