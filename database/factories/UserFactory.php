db263589da67f51b333b753576869a1d3878ff5c
Mickiel F. Oraa <mforaa@smg.local>
2022-12-06 02:12:31 +0800

    creation of factory for auth user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,8 +19,13 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+       
         'email' => $faker->unique()->safeEmail,
+        'first_name' => $faker->name,
+        'last_name' => $faker->lastname,
+        'contact_no' => $faker->e164PhoneNumber,,
+        'birthday' => now(),
+        'role_id' => 1,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
