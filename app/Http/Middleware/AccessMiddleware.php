f766a83c71c036a8cf2a81eb6edc7dea77c07f43
Miko <mickieloraa21@gmail.com>
2022-12-06 11:10:58 +0800

    add middleware

---
+++ app/Http/Middleware/AccessMiddleware.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class AccessMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        
+        $route_name = Route::getCurrentRoute()->getName();
+      
+        $exceptions = [
+           
+        ];
+        
+        if(in_array($route_name, $exceptions)){
+            return $next($request);
+        }
+
+        if($route_name == 'login'){
+            return $next($request);
+        }
+        if(\Auth::user()) {
+            if($route_name == 'logout'){
+                return $next($request);
+            }
+            return $next($request);
+           
+        }
+
+        return notAuthorizedResponse("You are not authorized to perform this operation " , 401);
+    }
+}
