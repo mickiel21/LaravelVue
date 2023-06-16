fd82c19a322fee53a897000831481d9496a28130
Miko <mickieloraa21@gmail.com>
2023-06-16 22:35:15 +0800

    add sample for deployment purpose

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::get('/miko/miko', function () {
+    return view('welcome');
+});
 
 Route::get('/{any}', function () {
     return view('master');
